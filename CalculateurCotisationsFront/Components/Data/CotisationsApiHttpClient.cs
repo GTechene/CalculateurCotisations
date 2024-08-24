0e0d78b5a27555167183442cca7a6e8c8673fd37
Guillaume Téchené <guillaume.techene@outlook.com>
2024-08-24 17:33:04 +0200

    Premier jet pour le front en Blazor

---
+++ CalculateurCotisationsFront/Components/Data/CotisationsApiHttpClient.cs
@@ -0,0 +1,38 @@
+﻿namespace CalculateurCotisationsFront.Components.Data;
+
+public class CotisationsApiHttpClient
+{
+    private readonly HttpClient _httpClient;
+
+    public CotisationsApiHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public Task<ResultatPrecisDeCotisations?> GetCotisations(decimal revenuNet)
+    {
+        return _httpClient.GetFromJsonAsync<ResultatPrecisDeCotisations>($"/cotisations/precises/{revenuNet}");
+    }
+}
+
+public record ResultatPrecisDeCotisations(
+    decimal MaladieHorsIndemnitesJournalieres,
+    decimal MaladieIndemnitesJournalieres,
+    decimal RetraiteDeBase,
+    decimal RetraiteComplementaire,
+    decimal InvaliditeDeces,
+    decimal AllocationsFamiliales,
+    decimal TotalCotisationsObligatoires,
+    decimal CSGNonDeductible,
+    decimal CSGDeductible,
+    decimal CRDS,
+    decimal FormationProfessionnelle,
+    decimal GrandTotal);
+
+
+public record CotisationsApiOptions
+{
+    public const string SectionName = "CotisationsApi";
+
+    public Uri Uri { get; set; } = null!;
+}
