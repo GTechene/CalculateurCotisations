592f85b3f9ab75ae958098be805c858f49c63af8
Guillaume Téchené <guillaume.techene@outlook.com>
2025-02-24 17:58:29 +0100

    On enlève Verify du test end-to-end, trop rigide. On remplace par des tests moins violents.

--- Cotisations.Tests/EndToEnd/CotisationsPageShould.cs
+++ Cotisations.Tests/EndToEnd/CotisationsPageShould.cs
@@ -1,17 +1,11 @@
 ﻿using Cotisations.Tests.EndToEnd.Extensions;
 using Microsoft.Playwright.NUnit;
+using NFluent;
 
 namespace Cotisations.Tests.EndToEnd;
 
 public class CotisationsPageShould : PageTest
 {
-    private readonly VerifySettings _verifySettings = new();
-
-    public CotisationsPageShould()
-    {
-        _verifySettings.UseDirectory("Snapshots");
-    }
-
     [Test]
     public async Task Affiche_les_cotisations_correctes_dans_le_tableau()
     {
@@ -20,6 +14,33 @@ public class CotisationsPageShould : PageTest
         await Page.GotoAsync(api.ServerAddress);
         await Page.SubmitCotisationsForm(50000, 2024);
         
-        await Verify(Page, _verifySettings);
+        var valeurCotisationsMaladie = await RecupereLaValeurDansLeTableau("Cotisations maladie (hors indemnités)");
+        Check.That(valeurCotisationsMaladie).IsEqualTo(3483);
+        var indemnitesMaladie = await RecupereLaValeurDansLeTableau("Cotisations indemnités maladie");
+        Check.That(indemnitesMaladie).IsEqualTo(260);
+        var retraiteDeBase = await RecupereLaValeurDansLeTableau("Retraite de base");
+        Check.That(retraiteDeBase).IsEqualTo(8264);
+        var retraiteComplementaire = await RecupereLaValeurDansLeTableau("Retraite complémentaire");
+        Check.That(retraiteComplementaire).IsEqualTo(3729);
+        var invaliditeDeces = await RecupereLaValeurDansLeTableau("Invalidité/décès");
+        Check.That(invaliditeDeces).IsEqualTo(603);
+        var allocationsFamiliales = await RecupereLaValeurDansLeTableau("Allocations familiales");
+        Check.That(allocationsFamiliales).IsEqualTo(113);
+        var csgDeductible = await RecupereLaValeurDansLeTableau("CSG déductible");
+        Check.That(csgDeductible).IsEqualTo(4654);
+        var csgNonDeductible = await RecupereLaValeurDansLeTableau("CSG non déductible");
+        Check.That(csgNonDeductible).IsEqualTo(1642);
+        var crds = await RecupereLaValeurDansLeTableau("CRDS");
+        Check.That(crds).IsEqualTo(342);
+        var formationProfessionnelle = await RecupereLaValeurDansLeTableau("Formation professionnelle");
+        Check.That(formationProfessionnelle).IsEqualTo(116);
+    }
+
+    private async Task<decimal> RecupereLaValeurDansLeTableau(string nomDeLaLigne)
+    {
+        var cellules = await Page.Locator($"tr:has-text('{nomDeLaLigne}')").First.Locator("td").AllAsync();
+        var valeurEnTexte = await cellules[1].TextContentAsync();
+        var valeur = decimal.Parse(valeurEnTexte!.Replace("€", "").Replace(" ", ""));
+        return valeur;
     }
 }
