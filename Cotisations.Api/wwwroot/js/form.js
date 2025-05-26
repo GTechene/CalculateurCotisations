6d3d600316c452528221d178c0daa8b0f003f3c4
Guillaume Téchené <guillaume.techene@outlook.com>
2025-05-26 18:07:53 +0200

    Ajout de la prise en compte des paramètres dans la query string de l'URL. Prépare la possibilité de bookmarker une simulation.

--- Cotisations.Api/wwwroot/js/form.js
+++ Cotisations.Api/wwwroot/js/form.js
@@ -1,5 +1,5 @@
-function initFormulaireCotisations() {
-  return {
+document.addEventListener('alpine:init', () => {
+  Alpine.data('gestionDuFormulaire', () => ({
     formData: {
       revenuNet: 0,
       annee: 2024,
@@ -23,6 +23,30 @@ function initFormulaireCotisations() {
       formationProfessionnelle: { valeur: 0, explication: "" },
       grandTotal: 0,
     },
+
+    init() {
+      const urlParams = new URLSearchParams(window.location.search);
+      
+      if (urlParams.has('revenuNet')) {
+        this.formData.revenuNet = urlParams.get('revenuNet');
+      }      
+      
+      if (urlParams.has('annee')) {
+        this.formData.annee = urlParams.get('annee');
+      }
+
+      if (urlParams.has('cotisationsFacultatives')) {
+        this.formData.cotisationsFacultatives = urlParams.get('cotisationsFacultatives');
+      }
+            
+      if (urlParams.has('revenuNet')) {
+        // Use nextTick to ensure the component is fully rendered before submitting
+        Alpine.nextTick(() => {
+          this.submitForm();
+        });
+      }
+    },
+
     submitForm() {
       const formData = this.formData;
       const url = `/cotisations/v2/precises/${formData.revenuNet}?annee=${formData.annee}&cotisationsFacultatives=${formData.cotisationsFacultatives}`;
@@ -47,5 +71,5 @@ function initFormulaireCotisations() {
           console.error("Error fetching data:", error);
         });
     },
-  };
-}
+  }));
+});
