cc4833ed4b4fec68714e04806844a828a4b2205f
Guillaume Téchené <guillaume.techene@outlook.com>
2026-09-23 18:12:45 +0200

    Correction du bouton permettant de copier le lien.

--- Cotisations.Api/wwwroot/js/copyUrl.js
+++ Cotisations.Api/wwwroot/js/copyUrl.js
@@ -1,18 +1,20 @@
-Alpine.data('urlCopier', () => ({
-  copyUrl: false,
-
-  copyCurrentUrl() {
-    if (navigator.clipboard && navigator.clipboard.writeText) {
-      navigator.clipboard.writeText(window.location.href)
-        .then(() => {
-          this.copyUrl = true;
-          setTimeout(() => {
-            this.copyUrl = false;
-          }, 2000);
-        })
-        .catch((err) => {
-          console.error('Failed to copy URL: ', err);
-        });
-    }
-  }
-}));
+document.addEventListener('alpine:init', () => {
+    Alpine.data('urlCopier', () => ({
+      copyUrl: false,
+    
+      copyCurrentUrl() {
+        if (navigator.clipboard && navigator.clipboard.writeText) {
+          navigator.clipboard.writeText(window.location.href)
+            .then(() => {
+              this.copyUrl = true;
+              setTimeout(() => {
+                this.copyUrl = false;
+              }, 2000);
+            })
+            .catch((err) => {
+              console.error('Failed to copy URL: ', err);
+            });
+        }
+      }
+    }));
+});
